8817cfedf9236a6b5ec90049a9bf65bb9c8393e0
AnsjDev <ansjdev@gmail.com>
2025-09-24 09:32:27 -0300

    fix: Correção syntax Somador4Bits

--- Somador4Bits.v
+++ Somador4Bits.v
@@ -11,4 +11,4 @@ module Somador4Bits(S, Cout, A, B, Cin);
     Somador1Bit bit1(S[1], C2, A[1], B[1], C1);
     Somador1Bit bit2(S[2], C3, A[2], B[2], C2);
     Somador1Bit bit3(S[3], Cout, A[3], B[3], C3);
-endmodulex''
+endmodule
