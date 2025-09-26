6e9b2fe1e20888ee12b2bffd29fa3358b814ef51
Elmer Carvalho <elmercarvalhofilho@gmail.com>
2025-09-26 05:48:32 -0300

    refactor: altera sintaxe para puramente estrutural

--- Subtrator1Bit.v
+++ Subtrator1Bit.v
@@ -2,10 +2,15 @@ module Subtrator1Bit(S, Bout, A, B, Bin);
    input A, B, Bin;
    output S, Bout;
    wire T1, T2, T3;
+   wire notA, notT1; // Wires para as saídas dos inversores
+
+   // Instancia inversores para A e T1
+   not invA(notA, A);
+   not invT1(notT1, T1);
 
    xor Xor0(T1, A, B);
-   and And0(T2, ~A, B);
-   and And1(T3, ~T1, Bin);
+   and And0(T2, notA, B); // Usa o fio invertido notA
+   and And1(T3, notT1, Bin); // Usa o fio invertido notT1
    or Or0(Bout, T2, T3);
    xor Xor1 (S, T1, Bin);
 endmodule
