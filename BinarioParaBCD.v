9b385a7ad9a23651940dfbee687a89267cbdd81e
Elmer Carvalho <elmercarvalhofilho@gmail.com>
2025-09-26 05:45:33 -0300

    fix: altera sintaxe para criar fio gnd e ser totalmente estrutural

--- BinarioParaBCD.v
+++ BinarioParaBCD.v
@@ -6,8 +6,7 @@ module BinarioParaBCD(
     output [3:0] bcd_dezenas,
     output [3:0] bcd_unidades
 );
-    // Wires para cada estágio do shift-and-add
-    wire [3:0] u0, d0, c0; // Estágio inicial
+    wire [3:0] u0, d0, c0;
     wire [3:0] u1_adj, d1_adj, c1_adj; wire [3:0] u1, d1, c1;
     wire [3:0] u2_adj, d2_adj, c2_adj; wire [3:0] u2, d2, c2;
     wire [3:0] u3_adj, d3_adj, c3_adj; wire [3:0] u3, d3, c3;
@@ -17,24 +16,28 @@ module BinarioParaBCD(
     wire [3:0] u7_adj, d7_adj, c7_adj; wire [3:0] u7, d7, c7;
     wire [3:0] u8_adj, d8_adj, c8_adj;
 
-    // Estágio 0: Inicialização com zeros
-    buf buf_u00(u0[0], 1'b0); buf buf_u01(u0[1], 1'b0); buf buf_u02(u0[2], 1'b0); buf buf_u03(u0[3], 1'b0);
-    buf buf_d00(d0[0], 1'b0); buf buf_d01(d0[1], 1'b0); buf buf_d02(d0[2], 1'b0); buf buf_d03(d0[3], 1'b0);
-    buf buf_c00(c0[0], 1'b0); buf buf_c01(c0[1], 1'b0); buf buf_c02(c0[2], 1'b0); buf buf_c03(c0[3], 1'b0);
+    // --- Geração Estrutural de Constante GND ---
+    wire gnd, temp_gnd;
+    not gnd_not_inst(temp_gnd, bin_in[0]);
+    and gnd_and_inst(gnd, bin_in[0], temp_gnd);
 
-    // --- Ciclo 1 (para bin_in[7]) ---
+    // Estágio 0: Inicialização com zeros
+    buf buf_u00(u0[0], gnd); buf buf_u01(u0[1], gnd); buf buf_u02(u0[2], gnd); buf buf_u03(u0[3], gnd);
+    buf buf_d00(d0[0], gnd); buf buf_d01(d0[1], gnd); buf buf_d02(d0[2], gnd); buf buf_d03(d0[3], gnd);
+    buf buf_c00(c0[0], gnd); buf buf_c01(c0[1], gnd); buf buf_c02(c0[2], gnd); buf buf_c03(c0[3], gnd);
+
+    // --- Ciclos 1 a 8 (sem alteração na lógica, apenas na inicialização acima) ---
     Add3_if_GTE5 adj1_u(u0, u1_adj); Add3_if_GTE5 adj1_d(d0, d1_adj); Add3_if_GTE5 adj1_c(c0, c1_adj);
     buf s1_c3(c1[3], c1_adj[2]); buf s1_c2(c1[2], c1_adj[1]); buf s1_c1(c1[1], c1_adj[0]); buf s1_c0(c1[0], d1_adj[3]);
     buf s1_d3(d1[3], d1_adj[2]); buf s1_d2(d1[2], d1_adj[1]); buf s1_d1(d1[1], d1_adj[0]); buf s1_d0(d1[0], u1_adj[3]);
     buf s1_u3(u1[3], u1_adj[2]); buf s1_u2(u1[2], u1_adj[1]); buf s1_u1(u1[1], u1_adj[0]); buf s1_u0(u1[0], bin_in[7]);
     
-    // --- Ciclo 2 (para bin_in[6]) ---
     Add3_if_GTE5 adj2_u(u1, u2_adj); Add3_if_GTE5 adj2_d(d1, d2_adj); Add3_if_GTE5 adj2_c(c1, c2_adj);
     buf s2_c3(c2[3], c2_adj[2]); buf s2_c2(c2[2], c2_adj[1]); buf s2_c1(c2[1], c2_adj[0]); buf s2_c0(c2[0], d2_adj[3]);
     buf s2_d3(d2[3], d2_adj[2]); buf s2_d2(d2[2], d2_adj[1]); buf s2_d1(d2[1], d2_adj[0]); buf s2_d0(d2[0], u2_adj[3]);
     buf s2_u3(u2[3], u2_adj[2]); buf s2_u2(u2[2], u2_adj[1]); buf s2_u1(u2[1], u2_adj[0]); buf s2_u0(u2[0], bin_in[6]);
 
-    // ... (O padrão se repete para os ciclos 3 a 7) ...
+    // ... (ciclos 3 a 7) ...
     Add3_if_GTE5 adj3_u(u2, u3_adj); Add3_if_GTE5 adj3_d(d2, d3_adj); Add3_if_GTE5 adj3_c(c2, c3_adj);
     buf s3_c3(c3[3], c3_adj[2]); buf s3_c2(c3[2], c3_adj[1]); buf s3_c1(c3[1], c3_adj[0]); buf s3_c0(c3[0], d3_adj[3]);
     buf s3_d3(d3[3], d3_adj[2]); buf s3_d2(d3[2], d3_adj[1]); buf s3_d1(d3[1], d3_adj[0]); buf s3_d0(d3[0], u3_adj[3]);
@@ -60,11 +63,9 @@ module BinarioParaBCD(
     buf s7_d3(d7[3], d7_adj[2]); buf s7_d2(d7[2], d7_adj[1]); buf s7_d1(d7[1], d7_adj[0]); buf s7_d0(d7[0], u7_adj[3]);
     buf s7_u3(u7[3], u7_adj[2]); buf s7_u2(u7[2], u7_adj[1]); buf s7_u1(u7[1], u7_adj[0]); buf s7_u0(u7[0], bin_in[1]);
 
-    // --- Ciclo 8 (para bin_in[0]) ---
+    // Último ciclo
     Add3_if_GTE5 adj8_u(u7, u8_adj); Add3_if_GTE5 adj8_d(d7, d8_adj); Add3_if_GTE5 adj8_c(c7, c8_adj);
-    // Último shift
     buf s8_c3(bcd_centenas[3], c8_adj[2]); buf s8_c2(bcd_centenas[2], c8_adj[1]); buf s8_c1(bcd_centenas[1], c8_adj[0]); buf s8_c0(bcd_centenas[0], d8_adj[3]);
     buf s8_d3(bcd_dezenas[3], d8_adj[2]);  buf s8_d2(bcd_dezenas[2], d8_adj[1]);  buf s8_d1(bcd_dezenas[1], d8_adj[0]);  buf s8_d0(bcd_dezenas[0], u8_adj[3]);
     buf s8_u3(bcd_unidades[3], u8_adj[2]); buf s8_u2(bcd_unidades[2], u8_adj[1]); buf s8_u1(bcd_unidades[1], u8_adj[0]); buf s8_u0(bcd_unidades[0], bin_in[0]);
-
 endmodule
--- Divisor4Bits.v
+++ Divisor4Bits.v
@@ -1,13 +1,9 @@
 // Módulo para divisão de 4x4 bits em estilo ESTRUTURAL
-// Utiliza o algoritmo de Divisão com Restauração (combinacional)
 module Divisor4Bits(
-    input [3:0] A,  // Dividendo
-    input [3:0] B,  // Divisor
-    output [3:0] Q, // Quociente
-    output [3:0] R, // Resto
-    output ERR      // Flag de erro (1 para divisão por zero)
+    input [3:0] A, input [3:0] B,
+    output [3:0] Q, output [3:0] R,
+    output ERR
 );
-    // --- Sinais Intermediários ---
     wire [4:0] R_partial1_shifted, R_partial1_sub, R_partial1;
     wire [4:0] R_partial2_shifted, R_partial2_sub, R_partial2;
     wire [4:0] R_partial3_shifted, R_partial3_sub, R_partial3;
@@ -15,35 +11,37 @@ module Divisor4Bits(
     wire [3:0] sub1_s, sub2_s, sub3_s, sub4_s;
     wire bout1, bout2, bout3, bout4;
 
+    // --- Geração de Constante GND ---
+    wire gnd, temp_gnd;
+    not gnd_not_inst(temp_gnd, A[0]);
+    and gnd_and_inst(gnd, A[0], temp_gnd);
+
     // --- Lógica de Erro (B == 0) ---
     wire nor_err_in;
     or or_err(nor_err_in, B[0], B[1], B[2], B[3]);
     not not_err(ERR, nor_err_in);
 
     // --- Estágio 1 (para Q[3]) ---
-    // R_partial1_shifted = {1'b0, A[3]} (Resto inicial é 0)
-    buf buf_s1_4(R_partial1_shifted[4], 1'b0); buf buf_s1_3(R_partial1_shifted[3], 1'b0); buf buf_s1_2(R_partial1_shifted[2], 1'b0); buf buf_s1_1(R_partial1_shifted[1], 1'b0); buf buf_s1_0(R_partial1_shifted[0], A[3]);
+    buf buf_s1_4(R_partial1_shifted[4], gnd); buf buf_s1_3(R_partial1_shifted[3], gnd); buf buf_s1_2(R_partial1_shifted[2], gnd); buf buf_s1_1(R_partial1_shifted[1], gnd); buf buf_s1_0(R_partial1_shifted[0], A[3]);
     Subtrator4Bits SUB1(sub1_s, bout1, R_partial1_shifted[3:0], B, R_partial1_shifted[4]);
     buf buf_sub1_4(R_partial1_sub[4], bout1); buf buf_sub1_3(R_partial1_sub[3], sub1_s[3]); buf buf_sub1_2(R_partial1_sub[2], sub1_s[2]); buf buf_sub1_1(R_partial1_sub[1], sub1_s[1]); buf buf_sub1_0(R_partial1_sub[0], sub1_s[0]);
     not not_q3(Q[3], bout1);
     Mux2para1_5bit MUX1(R_partial1_sub, R_partial1_shifted, bout1, R_partial1);
 
     // --- Estágio 2 (para Q[2]) ---
-    // R_partial2_shifted = {R_partial1[3:0], A[2]}
     buf buf_s2_4(R_partial2_shifted[4], R_partial1[3]); buf buf_s2_3(R_partial2_shifted[3], R_partial1[2]); buf buf_s2_2(R_partial2_shifted[2], R_partial1[1]); buf buf_s2_1(R_partial2_shifted[1], R_partial1[0]); buf buf_s2_0(R_partial2_shifted[0], A[2]);
     Subtrator4Bits SUB2(sub2_s, bout2, R_partial2_shifted[3:0], B, R_partial2_shifted[4]);
     buf buf_sub2_4(R_partial2_sub[4], bout2); buf buf_sub2_3(R_partial2_sub[3], sub2_s[3]); buf buf_sub2_2(R_partial2_sub[2], sub2_s[2]); buf buf_sub2_1(R_partial2_sub[1], sub2_s[1]); buf buf_sub2_0(R_partial2_sub[0], sub2_s[0]);
     not not_q2(Q[2], bout2);
     Mux2para1_5bit MUX2(R_partial2_sub, R_partial2_shifted, bout2, R_partial2);
-
-    // --- Estágio 3 (para Q[1]) ---
+    
+    // --- Estágios 3 e 4 (sem alteração) ---
     buf buf_s3_4(R_partial3_shifted[4], R_partial2[3]); buf buf_s3_3(R_partial3_shifted[3], R_partial2[2]); buf buf_s3_2(R_partial3_shifted[2], R_partial2[1]); buf buf_s3_1(R_partial3_shifted[1], R_partial2[0]); buf buf_s3_0(R_partial3_shifted[0], A[1]);
     Subtrator4Bits SUB3(sub3_s, bout3, R_partial3_shifted[3:0], B, R_partial3_shifted[4]);
     buf buf_sub3_4(R_partial3_sub[4], bout3); buf buf_sub3_3(R_partial3_sub[3], sub3_s[3]); buf buf_sub3_2(R_partial3_sub[2], sub3_s[2]); buf buf_sub3_1(R_partial3_sub[1], sub3_s[1]); buf buf_sub3_0(R_partial3_sub[0], sub3_s[0]);
     not not_q1(Q[1], bout3);
     Mux2para1_5bit MUX3(R_partial3_sub, R_partial3_shifted, bout3, R_partial3);
     
-    // --- Estágio 4 (para Q[0]) ---
     buf buf_s4_4(R_partial4_shifted[4], R_partial3[3]); buf buf_s4_3(R_partial4_shifted[3], R_partial3[2]); buf buf_s4_2(R_partial4_shifted[2], R_partial3[1]); buf buf_s4_1(R_partial4_shifted[1], R_partial3[0]); buf buf_s4_0(R_partial4_shifted[0], A[0]);
     Subtrator4Bits SUB4(sub4_s, bout4, R_partial4_shifted[3:0], B, R_partial4_shifted[4]);
     buf buf_sub4_4(R_partial4_sub[4], bout4); buf buf_sub4_3(R_partial4_sub[3], sub4_s[3]); buf buf_sub4_2(R_partial4_sub[2], sub4_s[2]); buf buf_sub4_1(R_partial4_sub[1], sub4_s[1]); buf buf_sub4_0(R_partial4_sub[0], sub4_s[0]);
